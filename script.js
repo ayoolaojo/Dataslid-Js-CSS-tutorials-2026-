e46a2dd498ba5c109de96dbdc80b9ff87acee24b
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-17 12:32:54 +0100

    Tutorials

---
+++ script.js
@@ -0,0 +1,65 @@
+// document.getElementById('heading').innerText = 'Hello Dataslid'
+
+// const message = 'This is my first Javascript CODE'
+
+// console.log(message)
+
+// // alert('welcome to Javascript')
+
+
+
+// const num = 9 
+
+// const myName = '9'
+
+// console.log('Ayoola')
+
+// let num1 = 0
+//  num1 = 5
+
+
+//  let nickName = 'AY'
+
+//  console.log(nickName)
+
+//  nickName = 'AZ'
+
+//  console.log(nickName)
+
+
+//  const num2 = 5;
+//  const num3 = 10
+
+//  const num4 = num3*num2
+//  console.log(num4)
+
+//  console.log(num3/num2)
+
+
+ let  myName = 'Ayoola'
+ console.log(myName)
+
+ myName =  'Paul'
+ console.log(myName)
+
+ myName = 'Mubarak'
+ console.log(myName)
+
+ 
+ const price = 1000
+ const discount = 200
+
+ const finalPrice = price - discount
+
+ console.log(finalPrice)
+
+
+
+ 
+
+
+
+
+
+
+
