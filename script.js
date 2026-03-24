05e7139182cd1c37d12a7fcb6042f0be1bd3c229
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-24 12:30:43 +0100

    update

--- script.js
+++ script.js
@@ -55,6 +55,168 @@
 
 
 
+ const fruit =  'orange'
+ console.log(fruit)
+
+ const email = 'ayoola@gmail.com'
+
+ const tool = ''
+
+ const myAge = 20
+ console.log(typeof(email))
+ console.log(typeof(myAge))
+ console.log(typeof(tool))
+
+ const largeNumber = 1234567890123456789012345n
+ console.log(typeof(largeNumber))
+
+ const isRainy = false
+ const isStudent = true
+
+ console.log(typeof(isRainy,isStudent))
+
+
+const Aisha = {
+    height: 7,
+    age : 18,
+    location: 'Ikirun',
+    email : 'aisha@gmail.com',
+    username: 'aisha001',
+    isStudent: true,
+}
+
+console.log(Aisha.location)
+
+const Car = {
+    make : 'Toyota',
+    model : 'camry',
+    year :  1999,
+    isNew : false,
+    color: 'white'
+}
+
+
+let vehicle;
+console.log(vehicle)
+vehicle = 'Venza'
+console.log()
+
+
+const player = {}    
+
+
+// Array data Type
+
+const ourNames = ['Ayooola','Paul', 'Ayesha','Quadri','Mubarak']
+const _ourNames = 'Ayoola Paul, Ayesha, qaudri, Mubarak'
+console.log(ourNames)
+console.log(_ourNames)
+
+
+const myText = 'Key subjects like Physics, Chemistry, Biology, Agricultural Science, Further Mathematics, and General Mathematics are included. Exams are to be conducted on a full Computer-Based Test (CBT)'
+
+
+console.log(ourNames[0])
+console.log(ourNames[4])
+console.log(ourNames[2])
+console.log(ourNames[3])
+console.log(ourNames[0])
+
+const vegetables = ['tomatoes','carrot','spinach']
+
+console.log(vegetables[1])
+
+
+const myMemo =  " It's my laptop"
+
+const test =  '10.5'
+const test1 =  10.5
+
+
+const food = ['rice', 'beans','yam','bread']
+const firstFood = food[0]
+console.log(firstFood)
+
+
+const lastFood = food[food.length - 1]
+console.log(lastFood)
+
+
+food[3] =  'pizza'
+console.log(food)
+
+ food.push('spagghetti')
+console.log(food)
+
+
+// =  to assign value
+// ==  
+// ===
+
+
+const z = '10'
+
+if (z == 10) {
+    console.log('yes')
+} else {
+    console.log("it's not 10")
+}
+
+
+console.log(2 == 2)
+console.log(2 == '2')
+console.log(2 === '2')
+
+
+console.log(2*2)
+
+
+const myExpo =  11 % 3
+console.log(myExpo)
+
+
+const myText1 =  'Hello'
+const myText2 =  'Dataslid'
+
+const greetings = myText1 + ' ' +  myText2
+console.log(greetings)
+
+let _myName = 'Data'
+_myName += 'Ola'
+
+console.log(_myName)
+
+
+let _myAge =  20
+_myAge++
+console.log(_myAge)
+
+
+let number1 =  200
+number1 --
+console.log(number1)
+
+console.log(1 != '1')
+
+
+const age = 18
+ if ( age > 17){
+    console.log('Welocme to Dataslid')
+ } else{
+    console.log('sorry! minors not allowed')
+ }
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 
