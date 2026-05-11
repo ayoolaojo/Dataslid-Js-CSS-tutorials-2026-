8e529ac3c43a528a1c44a393292005809e8fbfa0
Ayoola <johnsonayoolaojo@gmail.com>
2026-05-11 12:21:58 +0100

    new files added and updated

---
+++ new.js
@@ -0,0 +1,86 @@
+const myName = 'Ayoola';
+console.log(myName);
+
+
+const num1 =  10;
+const num2 = 10000;
+const total =  num1 + num2
+console.log(total)
+
+
+const car =  'Lexus'
+const car2 =  "Toyota"
+
+
+const message =  "it's Dataslid O'clock"
+console.log(message)
+
+
+const random = `he`
+
+ let scores;
+
+ scores =  70;
+
+
+
+
+function greet(name){
+    
+ console.log('welcome ' +  name)
+}
+
+
+greet('Fawaz')
+greet('Aaisha')
+greet('Paul')
+
+// Javascript Data Types
+//  String
+//  Object
+//  Boolean
+//  Number
+//  BigInt
+//  undefined
+//  null
+
+let school = 'UNIOSUN'
+
+console.log(typeof(school))
+
+
+const numero = 10.5
+
+
+let age  =  prompt('Enter your age')
+  age = Number(age)
+
+console.log(age)
+console.log(typeof(age))
+
+
+
+
+let number =  400
+number = String(number)
+console.log(typeof(number))
+
+
+let bigNumber =  1000000012n
+
+console.log(typeof(bigNumber))
+
+// boolean
+
+console.log(4>2)
+
+
+
+
+
+ 
+
+
+
+
+
