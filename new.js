6ddeeb6d1550aa32e2c756589df26c036aca3d99
Ayoola <johnsonayoolaojo@gmail.com>
2026-05-15 12:51:09 +0100

    new.js updated

--- new.js
+++ new.js
@@ -52,11 +52,11 @@ console.log(typeof(school))
 const numero = 10.5
 
 
-let age  =  prompt('Enter your age')
-  age = Number(age)
+// let age  =  prompt('Enter your age')
+//   age = Number(age)
 
-console.log(age)
-console.log(typeof(age))
+// console.log(age)
+// console.log(typeof(age))
 
 
 
@@ -73,6 +73,187 @@ console.log(typeof(bigNumber))
 // boolean
 
 console.log(4>2)
+
+const myString =  'lorem ipsum'
+console.log (myString.length)
+
+
+// Objects
+
+const Car = {
+   brand : 'Toyota',
+   model : 'corolla',
+   color : 'yellow',
+   year :  2019,
+   transmission : 'automatic'
+
+}
+
+
+console.log(Car)
+
+console.log(Car.color)
+
+console.log(Car.transmission)
+
+
+console.log(Car['brand'])
+
+
+const ourNames=  ['Ayoola', 'Imole', 'Aaishah', 'Taofeek', 'Mubarak', 'Daniel', 'AbdulBaqi', 'Qodri', 'Bobola', 'Paul', 30 , true, scores = {
+  Aaishah : 70,
+  Muideen : 50,
+  Ayoola : 40,
+
+}, 'Davo']
+
+
+
+
+console.log(ourNames)
+console.log(typeof(ourNames))
+
+
+const name1 = ourNames[0]
+console.log(name1)
+
+
+const lastName =  ourNames[ourNames.length -1 ]
+console.log(lastName)
+
+console.log(ourNames[4])
+ console.log(ourNames[8])
+ console.log(ourNames[6])
+ console.log(ourNames[3])
+ console.log(ourNames[5])
+ console.log(ourNames[2])
+ console.log(ourNames[9])
+ console.log(ourNames[12])
+
+
+ const aaishahScores = ourNames[12].Aaishah
+ console.log(aaishahScores)
+
+
+  // =   asign values
+  // ==   check equality
+
+  // ===   check equality and data type
+
+
+  // const myNumber =  10
+  // const $string =  '10'
+   console.log(10 == '10')
+  console.log(10 === '10')
+
+  const _numero = 20
+
+  console.log(_numero != 10)
+
+  const isStudent =  !false
+
+  console.log(isStudent)
+
+
+  let addition =  10 + 7
+  console.log(addition)
+
+  let word1 =  'Bolu '
+  let word2 =  'UniOsun'
+
+  let nickName = word1  +  word2
+  console.log(nickName)
+
+
+  // + - adds or concatenate
+  // -   substracts
+  // /    divides
+       
+  // *     multiplies
+
+  // %   modulus - returns raminder after division
+  
+ console.log(5%2)
+
+//  += 
+
+let xy =  25;
+
+  xy /= 5;
+
+  console.log(xy)
+
+
+  console.log(10**5)
+
+
+  // ++  - increment 
+  // --   decrement
+
+  let cores = 40;
+  cores --
+
+  console.log(cores)
+
+
+  // >=
+  // <=
+
+console.log( 2 <= 2)
+
+// ||  -   OR
+// !   -  NOT   
+// &&   - AND
+
+const desc = ` 
+
+
+
+
+
+
+ My name is Ayoola
+ I live in Osogbo
+ I'm a developer
+`
+
+console.log(desc)
+console.log(desc.length)
+console.log(desc.trim())
+
+console.log(desc.length)
+
+
+const myVariable =  '     Hello World'
+console.log(myVariable)
+
+console.log(myVariable.trim())
+
+
+console.log(myVariable.trim().charAt(0).toLowerCase())
+
+
+console.log(myVariable.at(-1))
+
+const gret =  'Congratulations'
+
+
+console.log(myVariable.concat(' of Javascript'))
+
+
+console.log(gret.slice(8))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
